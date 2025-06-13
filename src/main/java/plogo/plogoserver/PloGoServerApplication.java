b550f66a6afc78526f685a25d832549b2920959c
이예지 <oyatpeach@naver.com>
2025-06-13 14:50:34 +0900

    Initial commit

---
+++ src/main/java/plogo/plogoserver/PloGoServerApplication.java
@@ -0,0 +1,13 @@
+package plogo.plogoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PloGoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PloGoServerApplication.class, args);
+    }
+
+}
