2dbec33da404b13ec762b81194ecd32dabd22b53
이예지 <oyatpeach@naver.com>
2025-11-15 23:53:34 +0900

    [feat] 재배포

--- src/main/java/plogo/plogoserver/utils/BatchInitializer.java
+++ src/main/java/plogo/plogoserver/utils/BatchInitializer.java
@@ -5,18 +5,18 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import plogo.plogoserver.service.BatchService;
 
-@Component
-@RequiredArgsConstructor
-public class BatchInitializer implements CommandLineRunner {
-
-
-    private final BatchDataService batchDataService; // ✅ 수정됨
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("🚀 Batch Job Started...");
-        batchDataService.runSaveAll(); // ✅ 수정됨
-        System.out.println("✅ Batch Job Completed!");
-    }
-}
+//@Component
+//@RequiredArgsConstructor
+//public class BatchInitializer implements CommandLineRunner {
+//
+//
+//    private final BatchDataService batchDataService; // ✅ 수정됨
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        System.out.println("🚀 Batch Job Started...");
+//        batchDataService.runSaveAll(); // ✅ 수정됨
+//        System.out.println("✅ Batch Job Completed!");
+//    }
+//}
 
